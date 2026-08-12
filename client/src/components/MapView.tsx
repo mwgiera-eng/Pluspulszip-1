d7eed1c21d15944b22059c9ccebc827ebcd474bd
inDependBot <mwgiera@gmail.com>
2026-08-12 17:15:48 +0100

    Restore MapView.tsx before light map change

--- client/src/components/MapView.tsx
+++ client/src/components/MapView.tsx
@@ -613,7 +613,7 @@ export function MapView({ driverPosition }: MapViewProps) {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
 
         <MapController center={KRAKOW_COORDS} />
